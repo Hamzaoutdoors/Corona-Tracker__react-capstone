171e4d5afa332af6b308314c326ed7b834c57f92
Hamza <hamzaoutdoors@gmail.com>
2021-10-17 23:38:11 +0100

    add pagination

--- src/components/regions/Regions.js
+++ src/components/regions/Regions.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { shallowEqual, useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import Pagination from 'react-bootstrap/Pagination';
 import Region from './Region';
 import Header from '../Header';
 import Loading from '../Loading';
@@ -10,8 +11,8 @@ const Regiones = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
   const { countries, loaded } = useSelector((state) => state.countries, shallowEqual);
-
   const [regions, setRegions] = useState(null);
+  const [activePage, setActivePage] = useState(1);
 
   useEffect(() => {
     dispatch(fetchCountriesAction());
@@ -34,6 +35,11 @@ const Regiones = () => {
     }
   }, [id, loaded]);
 
+  useEffect(() => {
+    setActivePage(1);
+    return null;
+  }, [regions]);
+
   if (!regions) {
     return (
       <>
@@ -42,33 +48,73 @@ const Regiones = () => {
       </>
     );
   }
-  const regionsMoreAffected = regions.sort((a, b) => b.todayNewConfirmed - a.todayNewConfirmed);
+  const cardsPerPage = 6;
+  const lastPage = Math.ceil(regions.length / cardsPerPage);
+
   const total = regions.reduce((acc, curr) => acc + curr.todayNewConfirmed, 0);
-
-  const regionsList = regionsMoreAffected.map((region) => {
-    const {
-      id, name, todayNewConfirmed, todayNewDeaths,
-    } = region;
-
-    return (
-      <Region
-        key={id}
-        item={regions.indexOf(region)}
-        id={id}
-        name={name}
-        todayNewDeaths={todayNewDeaths}
-        todayNewConfirmed={todayNewConfirmed}
-      />
+  const renderList = () => {
+    const result = [];
+    for (
+      let i = (activePage - 1) * cardsPerPage;
+      i < Math.min(cardsPerPage * activePage, regions.length);
+      i += 1) {
+      result.push(
+        <Region
+          key={regions[i].id}
+          item={i}
+          id={regions[i].id}
+          name={regions[i].name}
+          todayNewDeaths={regions[i].todayNewDeaths}
+          todayNewConfirmed={regions[i].todayNewDeaths}
+        />,
+      );
+    }
+    return result;
+  };
+  const pagination = () => {
+    const items = [];
+    const item = (i) => (
+      <Pagination.Item key={`page-${i}`} active={activePage === i} onClick={() => setActivePage(i)} className="pageNum">
+        {i}
+      </Pagination.Item>
     );
-  });
+    if (lastPage < 8) {
+      for (let i = 1; i <= lastPage; i += 1) {
+        items.push(item(i));
+      }
+    } else {
+      items.push(item(1));
+      items.push(<Pagination.Ellipsis key="ellipsis-1" />);
+      if (activePage === 1 || activePage === 2) {
+        for (let i = 2; i <= 4; i += 1) {
+          items.push(item(i));
+        }
+      } else if (activePage === lastPage || activePage === lastPage - 1) {
+        for (let i = lastPage - 3; i <= lastPage - 1; i += 1) {
+          items.push(item(i));
+        }
+      } else {
+        for (let i = activePage - 1; i <= activePage + 1; i += 1) {
+          items.push(item(i));
+        }
+      }
+      items.push(<Pagination.Ellipsis key="ellipsis-2" />);
+      items.push(item(lastPage));
+    }
+
+    return items;
+  };
 
   return (
     <div>
       {' '}
       <Header total={`${total} Cases`} title={id.toUpperCase()} carteName={id} />
       <h3 className="middle-title">{`REGIONS BREAKDOWN - ${regions[0].date}`}</h3>
+      <Pagination className="d-flex justify-content-center my-3" size="sm" variant="warning">
+        {pagination()}
+      </Pagination>
       <div className="regions-container">
-        {regionsList}
+        {renderList()}
       </div>
     </div>
   );
